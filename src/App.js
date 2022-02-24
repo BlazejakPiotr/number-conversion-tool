ce8b4e8ccfd6f71734fdc446ed434c435bee422e
Piotr Błażejak <piotr.blazejak@yahoo.com>
2022-02-24 15:44:39 +0100

    basic layout

--- src/App.js
+++ src/App.js
@@ -1,23 +1,37 @@
-import logo from './logo.svg';
-import './App.css';
+import "bootstrap/dist/css/bootstrap.min.css";
+import { Button, Col, Container, Form, Row } from "react-bootstrap";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <Container>
+        <Row className="d-flex justify-content-center mt-5">
+          <Col md={12} lg={6}>
+            <h1>Number conversion tool</h1>
+
+            <Form className="mt-5">
+              <Form.Group
+                className="mb-3"
+                controlId="number"
+                className="text-start"
+              >
+                <div className="d-flex justify-content-evenly">
+                  <div className="w-75">
+                    <Form.Label>Enter a number to convert</Form.Label>
+                    <Form.Control type="number" placeholder="Enter number" />
+                  </div>
+                  <div className="d-flex align-items-end">
+                    <Button variant="danger" className="text-white">
+                      Convert
+                    </Button>
+                  </div>
+                </div>
+              </Form.Group>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
     </div>
   );
 }
